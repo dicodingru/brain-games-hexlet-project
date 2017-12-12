a4f7d772a20e025c50907c9801eacd41f2ed52a0
RDN <rdn@live.ru>
2017-12-12 19:07:00 +0300

    added asking for user name

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
 import { magenta, black } from 'colors';
+import { askUserName } from '..';
+
 console.log(`${magenta('Welcome')} to the ${black('Brain')} Games!`);
+
+const userName = askUserName();
+console.log(`Hello, ${userName}!`);
