28b4c2738e75d492a0c7d776fcdb1e95f3140762
RDN <rdn@live.ru>
2017-12-14 17:42:15 +0300

    step 7: added balance game

---
+++ src/games/balance.js
@@ -0,0 +1,12 @@
+import getRandomNumber, { getBalancedNumber } from '../utils';
+import { makeGame, run } from '../games/game';
+
+const desc = 'Balance the given number.';
+
+const funcQuestion = () => {
+  const question = getRandomNumber(10000);
+  const correct = getBalancedNumber(question);
+  return [question, correct];
+};
+
+export default () => run(makeGame(desc, funcQuestion));
--- src/utils.js
+++ src/utils.js
@@ -4,4 +4,16 @@ export const getEuclidGCD = (a, b) => {
   return getEuclidGCD(b, a % b);
 };
 
+export const getBalancedNumber = (num) => {
+  const numToArr = num.toString().split('').sort();
+  const iter = (arr) => {
+    const a = parseInt(arr.slice(0), 10);
+    const b = parseInt(arr.slice(-1), 10);
+    if (b - a < 2) return arr;
+    const newArr = [b - 1].concat(arr.slice(1, -1)).concat(a + 1).sort();
+    return iter(newArr);
+  };
+  return iter(numToArr).join('');
+};
+
 export default base => Math.floor(Math.random() * base);
