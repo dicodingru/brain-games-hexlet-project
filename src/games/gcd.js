b79142d61b44dde2f105257e62b75d46dc51b99a
RDN <rdn@live.ru>
2017-12-14 15:45:42 +0300

    step 6: added brain-gcd game

---
+++ src/games/gcd.js
@@ -0,0 +1,14 @@
+import getRandomNumber, { getEuclidGCD } from '../utils';
+import { makeGame, run } from '../games/game';
+
+const desc = 'Find the greatest common divisor of given numbers.';
+
+const funcQuestion = () => {
+  const a = getRandomNumber(100);
+  const b = getRandomNumber(100);
+  const question = `${a} ${b}`;
+  const correct = getEuclidGCD(a, b);
+  return [question, correct.toString()];
+};
+
+export default () => run(makeGame(desc, funcQuestion));
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,7 @@
+export const getEuclidGCD = (a, b) => {
+  if (a < b) return getEuclidGCD(b, a);
+  if (b === 0) return a;
+  return getEuclidGCD(b, a % b);
+};
+
 export default base => Math.floor(Math.random() * base);
