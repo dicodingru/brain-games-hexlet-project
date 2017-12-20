aeb24a303aebf71030fe9000858c49b43a3b3273
RDN <rdn@live.ru>
2017-12-20 16:53:37 +0300

    Fixed isPrime logic in utils.js

--- src/utils.js
+++ src/utils.js
@@ -22,11 +22,10 @@ export const isEven = number => !(number % 2);
 
 export const isPrime = (number) => {
   if (number < 2) return false;
-  if (number < 4) return true;
   const stop = number / 2;
   const iter = (counter) => {
-    if (number % counter === 0) return false;
     if (counter > stop) return true;
+    if (number % counter === 0) return false;
     return iter(counter + 1);
   };
   return iter(2);
